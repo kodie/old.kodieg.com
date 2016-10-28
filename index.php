3d454da4b9a21d03b3b854d81d165f11c2a4050f
Kodie Grantham <kodie.grantham@gmail.com>
2016-10-28 11:44:26 -0500

    Add StatCounter code

--- index.php
+++ index.php
@@ -49,5 +49,18 @@
   </ul>
 <?php win(ob_get_clean(), 'Projects', 'thumbs-up', 'projects', null); ?>
 
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=9355837;
+var sc_invisible=1;
+var sc_security="e87c5f5d";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<!-- End of StatCounter Code for Default Guide -->
+
 </body>
 </html>
